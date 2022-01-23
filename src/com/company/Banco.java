d4de885dc298ab59c0406a4b4101954113f70cbe
origin <marlon.dias@ufpe.br>
2022-01-23 02:11:27 -0300

    cleaning code

--- src/com/company/Banco.java
+++ src/com/company/Banco.java
@@ -8,12 +8,5 @@ import java.util.List;
 public class Banco {
 
     private String nome;
-    private List<Conta> contas;
-
-
-    public List<Conta> getContas() {
-        return contas;
-    }
-
 
 }
